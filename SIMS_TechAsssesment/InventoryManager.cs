dbb6457fb93160b2501cad76943bfc7ec7a8d12c
Czarina Angelica Kalimpo <cakalimpo@gmail.com>
2025-03-29 18:28:06 +0800

    comments added

--- SIMS_TechAsssesment/InventoryManager.cs
+++ SIMS_TechAsssesment/InventoryManager.cs
@@ -13,6 +13,11 @@ namespace SIMS_TechAsssesment
 
         public void AddProduct(Product product)
         {
+            // this Any() LINQ Method, check atleast one element if it matches:
+            // if the Product ID is already exists
+            // for easy understanding of lambda:
+                            // for each product symbolized as p compares the Product ID itself
+                            // if found, this will prevent duplicate entry of the product
             if(products.Any(p => product.ProductId == p.ProductId))
             {
                 Console.WriteLine("****************************");
@@ -26,40 +31,52 @@ namespace SIMS_TechAsssesment
         public void RemoveProduct(int productId)
         {
 
-            // 
+            // this FirstOrDefault() LINQ Method, check the first element that matches a condition 
+            // e.g: if the product contains with product ID exactly as user input. It will automatically
+                                                 // remove the product itself  
             var product = products.FirstOrDefault(p => p.ProductId == p.ProductId);
+            // this condition checks if the product is null
             if(product == null)
             {
                 Console.WriteLine("Product not found");
                 return;
             }
+            // otherwise, product will be removed
             products.Remove(product);
             Console.WriteLine("****************************");
             Console.WriteLine("Product removed successfully");
         }
         public void UpdateProduct(int productId, int newQuantity)
         {
+            // this condition checks newly entered quantity if it's less than zero
             if(newQuantity < 0)
             {
                 Console.WriteLine("Quantity cannot be negative");
             }
+            // this FirstOrDefault() LINQ Method, check the first element that matches a condition 
+            // e.g: if the product contains with product ID exactly as user input. It will update
+                                                     // the quantity of its product  
             var product = products.FirstOrDefault(p => p.ProductId == p.ProductId);
-            if(product == null)
+            // this condition checks if the product is null
+            if (product == null)
             {
                 Console.WriteLine("Product not found");
                 return;
             }
+            // therefore, the quantity will be updated 
             product.QuantityInStock = newQuantity;
             Console.WriteLine("****************************");
             Console.WriteLine("Product updated successfully!");
         }
         public void ListProduct()
         {
+            // this condition checks the product list, if empty then will return 
             if(!products.Any())
             {
                 Console.WriteLine("Inventory is empty");
                 return;
             }
+            // it will display the list of inventory that user input has
             Console.WriteLine("\n **** List of Inventory **** ");
             foreach(var product in products)
             {
@@ -68,6 +85,7 @@ namespace SIMS_TechAsssesment
         }
         public decimal GetTotalValue()
         {
+            // this return value display the total inventory by quantity and its price
             return products.Sum(p => p.QuantityInStock * p.Price);
         }
 
